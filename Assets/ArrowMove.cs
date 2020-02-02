4af6fa12cc4513fbf4b1c67ce49599f1f2db4f06
MMR <mamoru0523@gmail.com>
2020-02-02 15:36:18 +0900

    initial commit

---
+++ Assets/ArrowMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour
+{
+	int NumberOfCommands = 5;
+    int CurrentNumber = 0;
+    float PositionArrowY = 49f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+        	CurrentNumber = (CurrentNumber + 1) % NumberOfCommands;
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+        	CurrentNumber = (CurrentNumber + NumberOfCommands - 1) % NumberOfCommands;
+        }
+        PositionArrowY = 49 + CurrentNumber*29;
+
+		transform.position = new Vector3(transform.position.x, PositionArrowY, transform.position.z);
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Debug.Log(CurrentNumber);
+		}
+	}
+}
---
+++ Assets/BattleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleScript : MonoBehaviour
+{
+
+	int PlayerHP;
+	int EnemyHP;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerHP = 10;
+        EnemyHP = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
